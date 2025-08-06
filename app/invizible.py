d4fa23ad251ff5625b02a1e058e6e3622b3b2810
greatcoolge <121862222+greatcoolge@users.noreply.github.com>
2025-08-07 06:44:38 +0800

    invizible.py

--- app/invizible.py
+++ app/invizible.py
@@ -25,18 +25,18 @@ class InviZible(APPBase):
             
             # 生成规则文件
             with open(fileName, 'a') as f:
-                f.write("!\n")
-                if isLite:
-                    f.write("! Title: AdBlock InviZible Lite\n")
-                    f.write("! Description: 适用于 InviZible Pro、personalDNSfilter 的去广告合并规则，每 8 个小时更新一次。规则源：%s。Lite 版仅针对国内域名拦截。\n"%(self.sourceRule))
-                else:
-                    f.write("! Title: AdBlock InviZible\n")
-                    f.write("! Description: 适用于 InviZible Pro、personalDNSfilter 的去广告合并规则，每 8 个小时更新一次。规则源：%s。\n"%(self.sourceRule))
+                # f.write("!\n")
+                # if isLite:
+                #     f.write("! Title: AdBlock InviZible Lite\n")
+                #     f.write("! Description: 适用于 InviZible Pro、personalDNSfilter 的去广告合并规则，每 8 个小时更新一次。规则源：%s。Lite 版仅针对国内域名拦截。\n"%(self.sourceRule))
+                # else:
+                #     f.write("! Title: AdBlock InviZible\n")
+                #     f.write("! Description: 适用于 InviZible Pro、personalDNSfilter 的去广告合并规则，每 8 个小时更新一次。规则源：%s。\n"%(self.sourceRule))
                 f.write("! Homepage: %s\n"%(self.homepage))
                 f.write("! Source: %s/%s\n"%(self.source, os.path.basename(fileName)))
                 f.write("! Version: %s\n"%(self.version))
-                f.write("! Last modified: %s\n"%(self.time))
-                f.write("! Blocked domains: %s\n"%(len(blockList)))
+                f.write("! modified: %s\n"%(self.time))
+                f.write("! domains: %s\n"%(len(blockList)))
                 f.write("!\n")
                 for domain in blockList:
                     f.write("%s\n"%(domain))
